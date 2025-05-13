906a5376df2f0c145802f434fd3b530c82aa5946
Boneiverson <jimmytip2001@gmail.com>
2025-05-13 19:54:47 +0100

    update

--- src/pages/PropertiesPage.jsx
+++ src/pages/PropertiesPage.jsx
@@ -14,7 +14,7 @@ const PropertiesPage = () => {
             <Container
                 maxWidth="lg"
                 component="main"
-                sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+                sx={{ display: 'flex',  }}
             >
                 <PropertyPageContent  />
             </Container>
